78fee98e92bce284889a605726d733a4e924b128
CarlosNeimar <130559029+CarlosNeimar@users.noreply.github.com>
2023-09-19 14:53:53 -0300

    Update main.c

--- Unidade 3 - Exercícios Vetores/06/main.c
+++ Unidade 3 - Exercícios Vetores/06/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(void){
+  int n;
+  scanf("%d", &n);
+  int vet[n];
+  int positivos = 0, negativos = 0;
+  for (int i = 0; i < n; i++){
+    scanf("%d", &vet[i]);
+    if (vet[i] < 0){  
+    negativos++;
+    }
+    else{
+      positivos += vet[i];
+    }
+  }
+  printf("%d %d", negativos, positivos);
+  return 0;
 }
