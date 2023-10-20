e29f6f235c2717f52e0230506c370cd3ed6c6bf7
Pedro-nll <pedronll@outlook.com>
2023-10-20 14:37:36 -0300

    Questao 8 recursividade

--- Unidade 4 - Exercicios Recursividade/08/U04-Rec-Ex08.c
+++ Unidade 4 - Exercicios Recursividade/08/U04-Rec-Ex08.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+void inverteVetor(int vet[], int N){
+    if(N > 0){
+        printf("%d ", vet[N-1]);
+        inverteVetor(vet, N-1);
+    }
+}
+
 int main(void){
-    
+    int N; //TAMANHO DO VETOR
+    scanf("%d", &N);
+    int vet[N];
+    for(int i = 0; i < N; i++){
+        scanf("%d", vet + i);
+    }
+    inverteVetor(vet, N);
+    printf("\n");
     return 0;
 }
