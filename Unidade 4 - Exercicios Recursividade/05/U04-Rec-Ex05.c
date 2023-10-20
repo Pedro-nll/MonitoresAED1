9f32a82e8dce1f041fa2e557d0f65eff36d4b272
Pedro Henrique Pires Rodrigues <67903358+Credosvaldo@users.noreply.github.com>
2023-10-20 14:06:57 -0300

    U04-Rec-Ex05

---
+++ Unidade 4 - Exercicios Recursividade/05/U04-Rec-Ex05.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int n;
+int x;
+
+int qntsvezes(int v[], int indice)
+{
+    if(indice >= n)
+        return 0;
+
+    if(v[indice] == x)
+        return 1 + qntsvezes(v, indice + 1);
+
+    return qntsvezes(v, indice + 1);
+
+}
+
+int main()
+{
+    scanf("%d", &n);
+    int v[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &v[i]);
+    }
+
+    scanf("%d", &x);
+
+    printf("%d", qntsvezes(v, 0));
+
+    return 0;
+}
+
+
+/*
+10
+7 2 6 2 1 5 7 7 7 3
+7
+
+
+*/
